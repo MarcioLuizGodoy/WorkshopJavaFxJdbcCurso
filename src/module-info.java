7a6326cd8cee1e335f420f7a30d22b18d98f0e59
MarcioLuizGodoy <marciooliveirasilvagodoy@gmail.com>
2025-02-14 01:13:53 -0300

    DepartmentForm Dialog

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,7 @@ module WorkshopJavafxJdbc {
 	requires javafx.fxml;
 	requires javafx.graphics;
 	requires java.sql;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	//ESSAS DUAS LINHAS FORAM ADICIONADAS APOS UMA EXCESSAO. A CORREÇÃO VEIO DE DICA DO CHATGPT
